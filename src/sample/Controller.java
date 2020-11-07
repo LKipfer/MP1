061a81023f96569de0387c0bccb8efb7bc0ae1d2
LKipfer <72979191+LKipfer@users.noreply.github.com>
2020-11-07 13:14:10 +0100

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -8,10 +8,16 @@ import javafx.scene.control.Label;
 public class Controller {
     @FXML
     private Label lblMessage;
+    private Label lblTopLeft;
+    private Label lblTopRight;
+    private Label lblMidLeft;
+    private Label lblMidRight;
+    private Label lblBotLeft;
+    private Label lblBotRight;
     @FXML
     private Button btnOK;
 
-    public void handleBtnOK(ActionEvent actionEvent) {      //Was passiert, nachdem de chnopf druckt worde isch, glaubs..
+    public void handleBtnOK(ActionEvent actionEvent) {
         lblMessage.setText("k bye");
         btnOK.setText("Bye!");
     }
